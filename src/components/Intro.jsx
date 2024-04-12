9aaa35eb17c5ed10a2e0022cea3f0db0a490c846
JeongMyeongSik <jmsyaya@naver.com>
2024-04-13 01:12:29 +0900

    인트로 완료 및 헤더 수정

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+
+import jms from "../assets/img/jms.jpg";
+
+const introText = {
+  title:["Front-End","정명식"],
+  desc: ["talent is", "found at the end of the", "effort"],
+};
 
 const Intro = () => {
   return (
-    <div>Intro</div>
-  )
-}
+      <section id="intro">
+          <div className="intro__inner">
+              <h1 className="intro__title">{introText.title[0]}</h1>
+              <h2 className="intro__title2">프론트엔드&nbsp;개발자&ensp;<span className="intro__title3">정명식</span>&nbsp;입니다.</h2>
+              <div className="intro__lines" aria-hidden="true">
+                  <span className="line"></span>
+                  <span className="line"></span>
+                  <span className="line"></span>
+                  <span className="line"></span>
+                  <span className="line"></span>
+                  <span className="line"></span>
+                  <span className="line"></span>
+              </div>
+              <div className="intro__text">
+                  <div className="text">
+                      <div>{introText.desc[0]}</div>
+                      <div>{introText.desc[1]}</div>
+                      <div>{introText.desc[2]}</div>
+                  </div>
+                  <div className="img">
+                      <img src={jms} alt="어바웃" />
+                  </div>
+              </div>
+              <div className="intro__lines bottom" aria-hidden="true">
+                  <span className="line"></span>
+                  <span className="line"></span>
+                  <span className="line"></span>
+                  <span className="line"></span>
+                  <span className="line"></span>
+                  <span className="line"></span>
+                  <span className="line"></span>
+              </div>
+          </div>
+      </section>
+  );
+};
 
-export default Intro
+export default Intro;
